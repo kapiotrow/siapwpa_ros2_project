2605168d081cd9072e97b8ecf98569bb9b950424
Iwo <rymer@student.agh.edu.pl>
2025-12-03 16:20:45 +0000

    t

--- camera_package/launch/bridge.launch.py
+++ camera_package/launch/bridge.launch.py
@@ -10,9 +10,11 @@ def generate_launch_description():
             name='rgb_camera_bridge',
             output='screen',
             arguments=[
-                '/world/mecanum_drive/model/samochod/link/base_footprint/sensor/realsense_rgbd/image@sensor_msgs/msg/Image@gz.msgs.Image'
+                '/world/mecanum_drive/model/samochod/link/base_footprint/sensor/realsense_rgbd/image'
+                '@sensor_msgs/msg/Image@gz.msgs.Image'
             ]
         ),
+
         # Bridge dla Depth
         Node(
             package='ros_gz_bridge',
@@ -20,9 +22,11 @@ def generate_launch_description():
             name='depth_camera_bridge',
             output='screen',
             arguments=[
-                '/world/mecanum_drive/model/samochod/link/base_footprint/sensor/realsense_depth/depth_image@sensor_msgs/msg/Image@gz.msgs.Image'
+                '/world/mecanum_drive/model/samochod/link/base_footprint/sensor/realsense_depth/depth_image'
+                '@sensor_msgs/msg/Image@gz.msgs.Image'
             ]
         ),
+
         # Bridge dla cmd_vel
         Node(
             package='ros_gz_bridge',
@@ -33,16 +37,28 @@ def generate_launch_description():
                 '/model/samochod/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'
             ]
         ),
-        # Bridge dla Velodyne LiDAR
-        # Bridge dla Velodyne LiDAR
+
+        # Bridge dla Velodyne LiDAR LaserScan
         Node(
             package='ros_gz_bridge',
             executable='parameter_bridge',
-            name='velodyne_lidar_bridge',
+            name='velodyne_lidar_scan_bridge',
             output='screen',
             arguments=[
-                '/world/mecanum_drive/model/samochod/link/base_footprint/sensor/velodyne_lidar/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'
+                '/world/mecanum_drive/model/samochod/link/base_footprint/sensor/velodyne_lidar/scan'
+                '@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'
+            ]
+        ),
+
+        # Bridge dla Velodyne LiDAR PointCloud2 
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            name='velodyne_lidar_points_bridge',
+            output='screen',
+            arguments=[
+                '/world/mecanum_drive/model/samochod/link/base_footprint/sensor/velodyne_lidar/scan/points'
+                '@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked'
             ]
         )
-
     ])
