70d1695873aba2ad17f1c1654dea0796db1208f4
Your Name <you@example.com>
2025-12-09 13:08:44 +0000

    fixed sourceme

--- sourceme.sh
+++ sourceme.sh
@@ -1,4 +1,4 @@
 export GZ_SIM_RESOURCE_PATH="/home/developer/ros2_ws/src/models"
 
 . /opt/ros/humble/setup.bash
-. /home/developer/ros2_ws/install/setup.bash
+. /home/developer/ros2_ws/src/install/setup.bash
