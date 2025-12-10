e49b34b35c44260203b43b7235e7a15ed4171fed
Your Name <you@example.com>
2025-12-10 16:47:35 +0000

    added working launch

--- launch.sh
+++ launch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 tmux new-session \; \
-  send-keys "ros2 launch camera_package/launch/bridge.launch.py" C-m \; \
+  send-keys ". ./sourceme.sh $1; ros2 launch camera_package/launch/bridge.launch.py" C-m \; \
   split-window -h \; \
-  send-keys ". ./sourceme.sh; gz sim racetrack.sdf" C-m \; \
+  send-keys "sleep 1;. ./sourceme.sh $1; gz sim racetrack.sdf" C-m \; \
   split-window -v \; \
-  send-keys "python3 camera_package/camera_package/camera_pubsub.py" C-m \; \
+  send-keys "sleep 3; . ./sourceme.sh $1; python3 camera_package/camera_package/camera_pubsub.py" C-m \; \
   select-layout tiled
 
--- sourceme.sh
+++ sourceme.sh
@@ -2,3 +2,7 @@ export GZ_SIM_RESOURCE_PATH="/home/developer/ros2_ws/src/models"
 
 . /opt/ros/humble/setup.bash
 . /home/developer/ros2_ws/src/install/setup.bash
+
+export ROS_DOMAIN_ID=$1
+
+# pip3 install pynput 
