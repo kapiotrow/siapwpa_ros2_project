aa3efaf2a4b4bb14aaabec6e19b5fd8b17b738cf
Iwo <rymer@student.agh.edu.pl>
2025-12-17 16:43:33 +0000

    minor changes

--- sourceme.sh
+++ sourceme.sh
@@ -1,6 +1,6 @@
 export GZ_SIM_RESOURCE_PATH="/home/developer/ros2_ws/src/camera_package/models"
 
 . /opt/ros/humble/setup.bash
-. /home/developer/ros2_ws/src/install/setup.bash
+. /home/developer/ros2_ws/install/setup.bash
 
 ROS_DOMAIN_ID=$1
